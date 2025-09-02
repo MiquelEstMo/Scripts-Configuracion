d9ee907ad561809578f38085697f5c358fc1ad75
Miquel <54858840+MiquelEstMo@users.noreply.github.com>
2025-09-02 18:02:32 +0200

    Update vim-fullinstall.sh

--- vim-fullinstall.sh
+++ vim-fullinstall.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
 echo -e "\nInstalant NeoVim && NvChad"
 
-
 # --------- ACTUALITZACIÓ DE PAQUETS ---------
-
 echo -e "Actualitzant paquets locals...\n"
-
 sudo apt-get update
 sudo apt-get upgrade
-
 echo -e "\nPaquets actualitzats amb éxit."
 
-# --------- ACTUALITZACIÓ DE PAQUETS ---------
-
 # -------------------------- INSTALACIÓ DE NEOVIM --------------------------
-
 echo -e "\nInstalant NeoVim..."
-
-mkdir -p ~/Applications1
-cd ~/Applications1
-
+mkdir -p ~/Applications
+cd ~/Applications
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage
 chmod +x nvim-linux-x86_64.appimage
 sudo ln -s ~/Applications/nvim-linux-x86_64.appimage /usr/local/bin/nvim
-
 nvim --version
-
 echo "NeoVim instalat correctament!"
-
 # -------------------------- INSTALACIÓ DE NEOVIM --------------------------
